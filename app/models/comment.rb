a2a64686110b23658034bc8376d188d110f294b5
Shuhad786 <shuhad.loofer@gmail.com>
2023-01-26 17:19:51 +0200

    Added the methods and validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def update_comments_counter
+    self.update(comments_count: self.comments.count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,12 @@ class Post < ApplicationRecord
   validates :title, length: { maximum: 250 }
   validates :CommentsCounter, numericality: { greater_than_or_equal_to: 0 }
   validates :LikesCounter, numericality: { greater_than_or_equal_to: 0 }
+
+  def update_posts_count
+    self.update(posts_count: self.posts.count)
+  end
+
+  def recent_comments
+    self.comments.order(created_at: :desc).limit(5)
+  end
 end
