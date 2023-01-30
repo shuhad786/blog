50cc695a8be5f6ef54f0e50cbf00cbd2fadd7ef3
Shuhad786 <shuhad.loofer@gmail.com>
2023-01-30 14:30:45 +0200

    Added controllers and routes to the code.

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_115016) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.integer "CommentsCounter"
-    t.integer "LikesCounter"
+    t.integer "CommentsCounter", default: 0
+    t.integer "LikesCounter", default: 0
     t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_115016) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "PostCounter"
+    t.integer "PostCounter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
