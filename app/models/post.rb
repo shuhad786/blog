712d54c595f163a02cc67aa5ee0793a08e3fb8df
Shuhad786 <shuhad.loofer@gmail.com>
2023-02-02 16:10:21 +0200

    Create views and update controllers

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,4 +15,8 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  def all_comments
+    comments.order(created_at: :desc)
+  end
 end
