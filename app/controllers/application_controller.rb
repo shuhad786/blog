c9700b328c9a7fb0d58c8c1fcb618974aebcd683
Shuhad786 <shuhad.loofer@gmail.com>
2023-02-07 18:56:24 +0200

    Update code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    User.first
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password)}
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true
+  # validates :name, presence: true
   validates :PostCounter, numericality: { greater_than_or_equal_to: 0 }
 
   def find_recent_posts
