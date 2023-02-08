f687b3af755109b29971d5b853d79590eb031e0d
unknown <sharonkessio6@gmail.com>
2023-02-08 12:52:04 +0300

    route for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   root to: 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :create, :new] do
