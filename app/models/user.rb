cb3b0f21e351c3df5eb99d2c34c297a1768c02ee
Shuhad786 <shuhad.loofer@gmail.com>
2023-02-08 17:09:48 +0200

    Corrected linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable
-  
+
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
