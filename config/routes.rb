610062bd76cbdaa4c14db6763761d909cac81747
unknown <sharonkessio6@gmail.com>
2023-02-10 17:00:18 +0300

    update root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :update, :destroy] do
-      resources :posts, only: [:index, :show] do
-        resources :comments, only: [:create, :index]
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
       end
     end
   end
