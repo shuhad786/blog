f62a257402a0135a5c831755165f802c396a23d9
Shuhad786 <shuhad.loofer@gmail.com>
2023-02-10 18:16:06 +0200

    Corrected the code as per reviewers request

--- app/controllers/API/comments_controller.rb
+++ app/controllers/API/comments_controller.rb
@@ -5,8 +5,9 @@ class Api::CommentsController < ApplicationController
   end
 
   def create
+    current_user = params[:user_id]
     @comment = Comment.new(comment_params)
-    @comment.author_id = current_user.id
+    @comment.author_id = current_user
     @comment.post_id = params[:post_id]
     @post = Post.find(params[:post_id])
     @comment.save!
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :manage, :all if user.role == 'admin'
-    can :destroy, Post, author: user
-    can :destroy, Comment, author: user
+    user ||= User.new
+
+    if user.role == 'admin' 
+      can :manage, :all
+    else
+      can :manage, Post, author: user
+      can :manage, Comment, author: user
+      can :read, :all
+    end
   end
 end
